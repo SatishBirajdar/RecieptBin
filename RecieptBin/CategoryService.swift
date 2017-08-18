f5f24127693a5897a06569226e6b51cec4c0307f
Satish Birajdar <satishbirajdar1@gmail.com>
2017-08-18 14:49:17 -0400

    changes after failed overlapping

--- RecieptBin/CategoryService.swift
+++ RecieptBin/CategoryService.swift
@@ -11,31 +11,9 @@ import Foundation
 class CategoryService{
     
     func getCategories() -> Array <Category>{
-//        let categoryArray: Array <Category> = []
-        let categories = [Category(name:"Satish"), Category(name:"Nikhil")]
-        
+        let categories = [Category(name:"Satish"),
+                          Category(name:"Nikhil")]
+       
         return categories
-    }
-    
-//    func getCategories(callBack: @escaping ([Category]) -> Void){
-//        let categories = [Category(name:"Satish"), Category(name:"Nikhil")]
-//        
-//        let delayTime = DispatchTime.now(dispatch_time_t(DispatchTime.now()), Int64(2 * Double(NSEC_PER_SEC)))
-//        dispatch_after(delayTime, dispatch_get_main_queue()) {
-//            callBack(categories)
-//        }
-//    }
-//    
-//    func getUsers(callBack:([User]) -> Void){
-//        let users = [User(firstName: "Iyad", lastName: "Agha", email: "iyad@test.com", age: 36),
-//                     User(firstName: "Mila", lastName: "Haward", email: "mila@test.com", age: 24),
-//                     User(firstName: "Mark", lastName: "Astun", email: "mark@test.com", age: 39)
-//        ]
-//        
-//        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC)))
-//        dispatch_after(delayTime, dispatch_get_main_queue()) {
-//            callBack(users)
-//        }
-//    }
-    
+    }    
 }
--- RecieptBin/CategoryViewController.swift
+++ RecieptBin/CategoryViewController.swift
@@ -12,46 +12,17 @@ import UIKit
 class CategoryViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet var tableView: UITableView!
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//    }
-    
-    
-    
-//    @IBOutlet weak var emptyView: UIView?
-//    @IBOutlet weak var tableView: UITableView?
-//    @IBOutlet weak var activityIndicator: UIActivityIndicatorView?
-    
+  
     private let categoryPresenter = CategoryPresenter(categoryService: CategoryService())
     private var categorysToDisplay = [CategoryViewModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView?.dataSource = self
-//        activityIndicator?.hidesWhenStopped = true
         
         categoryPresenter.attachView(view: self as! CategoryPresenterView)
         categoryPresenter.getCategories()
     }
-    
-
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        var cell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
-//        
-//        return cell!
-//    }
-//    
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
-//    
-//    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-//    
-//    }
-
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categorysToDisplay.count
@@ -61,8 +32,6 @@ class CategoryViewController: UIViewController, UITableViewDataSource {
         let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "UserCell")
         let userViewData = categorysToDisplay[indexPath.row]
         cell.textLabel?.text = userViewData.name
-//        cell.detailTextLabel?.text = CategoryViewModel.name
-//        cell.textLabel
         return cell
     }
     
