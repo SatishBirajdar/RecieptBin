412b4b49be4ab7b22ff44130189384f85e889b09
Satish Birajdar <satishbirajdar1@gmail.com>
2017-09-18 21:33:43 -0400

    jj

--- Jenkinsfile.rb
+++ Jenkinsfile.rb
@@ -2,7 +2,12 @@ pipeline {
     agent any
     
     stages {
-        
+        stage('Build') {
+            steps {
+                echo 'Building..'
+                # withMaven(maven: 'maven_3.5.0')
+            }
+        }
         stage('Test') {
             steps {
                 echo 'Testing..'
